85239727b294c012770229e1686450c83ba61ce9
LeandroAzanniel <testemeiosangue@gmail.com>
2020-06-03 22:57:58 -0400

    Adicionado métodos update, delete e seeds de level e word

--- backend/src/controller/TeacherController.js
+++ backend/src/controller/TeacherController.js
@@ -22,5 +22,53 @@ module.exports = {
     } catch (error) {
       return res.status(409).json(error)
     }
+  },
+
+  async create (req, res) {
+    const { name, username, password } = req.body
+
+    try {
+      const [teacher] = await connection('teacher')
+        .insert({ name, username, password })
+
+      return res.status(200).json({ id: teacher })
+    } catch (error) {
+      return res.status(409).json(error)
+    }
+  },
+
+  async update (req, res) {
+    const { id } = req.params
+    const { name, username, password } = req.body
+
+    const [teacherSearch] = await connection('teacher')
+      .where('id', id)
+      .select('id')
+
+    if (!teacherSearch) { return res.status(404).json('Teacher not found') }
+
+    try {
+      await connection('teacher')
+        .where('id', id)
+        .update({ name, username, password })
+
+      const [teacher] = await connection('teacher').where('id', id).select()
+
+      return res.status(200).json(teacher)
+    } catch (error) {
+      return res.status(409).json(error)
+    }
+  },
+
+  async delete (req, res) {
+    const { id } = req.params
+
+    try {
+      await connection('teacher').where('id', id).del()
+
+      return res.status(204).send()
+    } catch (error) {
+      return res.status(444).json({ message: 'Something wrong was happening', error: error })
+    }
   }
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,6 +5,9 @@ const TeacherController = require('./controller/TeacherController')
 const routes = express.Router()
 
 routes.get('/teachers', TeacherController.index)
+routes.post('/teachers', TeacherController.create)
+routes.put('/teachers/:id', TeacherController.update)
 routes.get('/teachers/:id', TeacherController.show)
+routes.delete('/teachers/:id', TeacherController.delete)
 
 module.exports = routes
