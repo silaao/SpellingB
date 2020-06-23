3a01dc1d746f4d232bbc833d7ebdee9ad81c7127
silaao <silas_10d@hotmail.com>
2020-06-23 09:50:46 -0400

    Realizado a criação da página de sign in

--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -1,10 +1,45 @@
 import React from 'react';
+import bee from '../../assets/bee.png';
+import beespelling from '../../assets/beespelling.jpg';
 
-import { Container, Text } from './styles';
+import {
+  Container,
+  Content,
+  Header,
+  Image,
+  ImageBee,
+  Form,
+  Title,
+  Input,
+  ButtonSignIn,
+  Div,
+  ButtonSignUp,
+} from './styles';
 
 const SignIn: React.FC = () => (
   <Container>
-    <Text>Base construída</Text>
+    <Content>
+      <Header>
+        Spelling Bee
+        <Image src={bee} alt="bee" />
+      </Header>
+      <Div>
+        <ImageBee src={beespelling} alt="beespelling" />
+        <Form>
+
+          <Title>Sign In</Title>
+
+          <Input type="email" placeholder="Email" />
+          <Input type="password" placeholder="Senha" />
+
+          <ButtonSignIn>Entrar</ButtonSignIn>
+          <ButtonSignUp>
+            {/* <FiLogOut size={28} color="#fff" /> */}
+            Não tenho cadastro
+          </ButtonSignUp>
+        </Form>
+      </Div>
+    </Content>
   </Container>
 );
 
