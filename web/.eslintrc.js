fb320ab2744b11bdef3c23218080bc5a2620879a
LeandroAzanniel <leo.azannielttt@gmail.com>
2020-07-31 11:37:21 -0400

    🎨 Adicionada base para implementação com MaterialUI

---
+++ web/.eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'prettier',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'prettier',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.js', '.jsx'],
+      },
+    ],
+  },
+};
---
+++ web/src/App/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Box, Typography } from '@material-ui/core';
+
+import './styles.css';
+
+function App() {
+  return (
+    <Box>
+      <Typography variant="body2" color="textPrimary" align="center">Hello World</Typography>
+    </Box>
+  );
+}
+
+export default App;
